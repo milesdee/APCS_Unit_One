5e467ae021d25ced48d13329c1c2c17991d032e9
milesdee <miles.drabinski@ssfs.org>
2021-09-13 12:12:15 -0400

    did funny maths

--- src/CorrectChange.java
+++ src/CorrectChange.java
@@ -1,8 +1,40 @@
 import java.util.Scanner;
-
 public class CorrectChange {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int change = scan.nextInt();
+        System.out.println(change + " cents:");
+
+        int dollarcount = ((change - (change % 100 )) / 100);
+        System.out.println(dollarcount + " dollar(s)");
+
+        change = (change % 100);
+        System.out.println(change + " cents remaining");
+
+        int quartercount = ((change - (change % 25)) / 25);
+        System.out.println(quartercount + " quarter(s)");
+
+        change = (change % 25);
+        System.out.println(change + " cents remaining");
+
+        int dimecount = ((change - (change % 10)) / 10 );
+        System.out.println(dimecount + " dime(s)");
+
+        change = ( change % 10);
+        System.out.println(change + " cents remaining");
+
+        int nickelcount = ((change - (change % 5)) / 5);
+        System.out.println(nickelcount + " nickel(s)");
+
+        change = (change % 5);
+        System.out.println(change + " cents remaining");
+
+        int pennycount = change;
+        System.out.println(pennycount + " pennies");
+
+        change = (change - change);
+        System.out.println(change + " cents remaining");
 
     }
 }
--- src/DateFormat.java
+++ src/DateFormat.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class DateFormat {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("what month is it?");
+        String month = scan.nextLine();
+        System.out.println("what day is it?");
+        int day = scan.nextInt();
+        System.out.println("and what year?");
+        int year = scan.nextInt();
+        System.out.println("and what day of the week is it?");
+        String dayOfWeek = scan.nextLine();
 
+        System.out.println("So today is " + dayOfWeek + ", " + month + " " + day + ", " + year + ".");
     }
 }
