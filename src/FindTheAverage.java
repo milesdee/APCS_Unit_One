1d6734f612a0ec5bb3f50cd12d99816e9bb5b194
milesdee <miles.drabinski@ssfs.org>
2021-09-14 15:15:30 -0400

    average

--- src/FindTheAverage.java
+++ src/FindTheAverage.java
@@ -5,16 +5,27 @@ public class FindTheAverage {
     public static void main(String[] args) {
 
         // Create a Scanner object to get user input
-
+        Scanner scan = new Scanner(System.in);
         // Create a total variable to keep track of the total value
-
+        int total = 0;
         // Get four pieces of user input
-
+        System.out.println("Please give me four values.");
+        System.out.println("What's the first number?");
+        total += scan.nextInt();
+        System.out.println(total);
+        System.out.println("What's the second number?");
+        total += scan.nextInt();
+        System.out.println(total);
+        System.out.println("What's the third number?");
+        total += scan.nextInt();
+        System.out.println(total);
+        System.out.println("What's the fourth number?");
+        total += scan.nextInt();
+        System.out.println("Thank you!");
         // Calculate the average
-
+        double avg = (total/4);
         // Display the result
-
-
+        System.out.println("The average is " + avg + "!");
     }
 
 }
