5f545fb90e992737b5202e41d7fa3d7fe74369f4
milesdee <miles.drabinski@ssfs.org>
2021-09-16 08:31:30 -0400

    did AddOneTakeOne

--- src/AddOneTakeOne.java
+++ src/AddOneTakeOne.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class AddOneTakeOne {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give me a (whole) number: ");
+        int input = scan.nextInt();
+        int bigger = (input + 1);
+        int smaller = (input - 1);
+        System.out.println(input + "? Okay!");
+        System.out.println(input + " is 1 higher than " + (smaller) + ",");
+        System.out.println("and is 1 lower than " + (bigger) + ".");
     }
 }
