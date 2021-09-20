86fa9813305e01308d53395761cd8371ef414a20
milesdee <miles.drabinski@ssfs.org>
2021-09-20 15:23:27 -0400

    painting a room

--- src/PaintingARoom.java
+++ src/PaintingARoom.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class PaintingARoom {
 
     public static void main(String[] args) {
+        int paintBucket, doorsArea, windowsArea, subtractedArea;
+        double roomHeight, roomWidth, roomLength, wallsAnC, wallsBnD, ceilingArea, totalWallArea, neededPaint;
+        paintBucket = 350;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your room is square/rectangular. This is fact now.");
+        System.out.print("How long is your room, in feet?   ");
+        roomLength = scan.nextDouble();
+        System.out.print("How wide is your room, in feet?   ");
+        roomWidth = scan.nextDouble();
+        System.out.print("How high are your ceilings, in feet?   ");
+        roomHeight = scan.nextDouble();
 
+        wallsAnC = (roomLength * roomHeight);
+        wallsBnD = (roomWidth * roomHeight);
+        ceilingArea = (roomWidth * roomLength);
+
+        System.out.print("How many windows do you have?   ");
+        windowsArea = (10 * scan.nextInt());
+        System.out.print("How many doors do you have?   ");
+        doorsArea = (15 * scan.nextInt());
+
+        subtractedArea = (windowsArea + doorsArea);
+        totalWallArea = (wallsAnC + wallsBnD - subtractedArea);
+        neededPaint = (totalWallArea + ceilingArea);
+
+        System.out.println("The total area you are painting is " + (int)neededPaint + " square feet.");
+        System.out.println("A single commercial bucket of paint contains 350 square feet's worth of paint");
+        System.out.println("So you need " + (int)((neededPaint/paintBucket) + 1) + " buckets of paint.");
     }
 
 }
